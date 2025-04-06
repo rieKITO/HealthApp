1e8d27b37e6dc52428af48c8e0d65139396d9290
Alexander <potemkin_av7@mail.ru>
2025-04-06 19:03:08 +0300

    [Feature] Add a SleepRecordEditorView for editing or adding sleep data

---
+++ HealthApp/Core/Home/Views/SleepTrackerView/SleepHistoryView/SleepRecordEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  SleepRecordEditorView.swift
+//  HealthApp
+//
+//  Created by Александр Потёмкин on 06.04.2025.
+//
+
+import SwiftUI
+
+struct SleepRecordEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SleepRecordEditorView()
+}
