a54e1ca601dfb67b54a9befc02276db2bea90383
Alexander <potemkin_av7@mail.ru>
2025-04-14 15:07:29 +0300

    [Patch] Change formatDuration method in DateFormatterHelper
    Change returned string

--- HealthApp/Helpers/DateFormatterHelper.swift
+++ HealthApp/Helpers/DateFormatterHelper.swift
@@ -12,7 +12,7 @@ class DateFormatterHelper {
     static func formatDuration(_ duration: TimeInterval) -> String {
         let hours = Int(duration) / 3600
         let minutes = (Int(duration) % 3600) / 60
-        return "\(hours)H \(minutes)M"
+        return "\(hours)h \(minutes)m"
     }
     
     static func formatDate(_ date: Date) -> String {
@@ -27,4 +27,5 @@ class DateFormatterHelper {
         formatter.timeStyle = .short
         return formatter.string(from: date)
     }
+    
 }
