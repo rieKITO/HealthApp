6f24b18551c29705405bf010d820f245f85d15f8
Alexander <potemkin_av7@mail.ru>
2025-04-27 16:53:25 +0300

    [Feature] Add planners labels into the NutritionTrackerView

--- HealthApp/Core/NutritionTracker/Views/NutritionTrackerView.swift
+++ HealthApp/Core/NutritionTracker/Views/NutritionTrackerView.swift
@@ -9,26 +9,91 @@ import SwiftUI
 
 struct NutritionTrackerView: View {
     
+    // MARK: - View Model
+    
     @State
     private var viewModel = NutritionViewModel()
+    
+    // MARK: - Body
 
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                Text("Recipes:")
-                ForEach(viewModel.allRecipes) { recipe in
-                    VStack(alignment: .leading) {
-                        Text(recipe.title)
-                        Text("Калории: \(recipe.caloriesText)")
-                    }
-                    .onAppear {
-                        viewModel.loadMoreRecipesIfNeeded(currentItem: recipe)
-                    }
+        VStack {
+            HStack {
+                NavigationLink {
+                    
+                } label: {
+                    mealPlannerLabel
+                }
+                NavigationLink {
+                    
+                } label: {
+                    waterPlannerLabel
                 }
             }
+            .padding(.horizontal)
         }
     }
 }
+
+// MARK: - Subviews
+
+private extension NutritionTrackerView {
+    
+    private var mealPlannerLabel: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.theme.accent.opacity(0.5), lineWidth: 2)
+            .fill(Color.theme.background)
+            .frame(maxWidth: .infinity)
+            .frame(height: 180)
+            .overlay {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(systemName: "fork.knife")
+                            .font(.title2)
+                            .frame(width: 30, height: 30)
+                        Text("Meal Planner")
+                            .font(.headline)
+                        Spacer()
+                    }
+                    .foregroundStyle(Color.theme.accentGreen)
+                    Text("Plan your daily meals")
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(Color.theme.secondaryText)
+                    Spacer()
+                }
+                .padding(10)
+            }
+    }
+    
+    private var waterPlannerLabel: some View {
+        RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.theme.accent.opacity(0.5), lineWidth: 2)
+            .fill(Color.theme.background)
+            .frame(maxWidth: .infinity)
+            .frame(height: 180)
+            .overlay {
+                VStack(alignment: .leading) {
+                    HStack {
+                        Image(systemName: "mug.fill")
+                            .font(.title2)
+                            .frame(width: 30, height: 30)
+                        Text("Water Planner")
+                            .font(.headline)
+                        Spacer()
+                    }
+                    .foregroundStyle(Color.theme.accentGreen)
+                    Text("Track your hidration")
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(Color.theme.secondaryText)
+                    Spacer()
+                }
+                .padding(10)
+            }
+    }
+    
+}
+
+// MARK: - Preview
 
 #Preview("Light Mode") {
     NutritionTrackerView()
