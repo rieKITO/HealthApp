f02a87931a7ef618af10b7a032d00503ab5b62c1
Alexander <potemkin_av7@mail.ru>
2025-05-22 20:07:54 +0300

    [Feature] Add meal intake info view

--- HealthApp/Core/NutritionTracker/Views/MealPlannerViews/MealIntakeInfoView.swift
+++ HealthApp/Core/NutritionTracker/Views/MealPlannerViews/MealIntakeInfoView.swift
@@ -8,11 +8,121 @@
 import SwiftUI
 
 struct MealIntakeInfoView: View {
+    
+    // MARK: - Environment
+    
+    @Environment(\.dismiss)
+    private var dismiss
+    
+    @Environment(NutritionViewModel.self)
+    private var viewModel
+    
+    // MARK: - Private Properties
+    
+    private var totalCalories: Double {
+        recipes.reduce(0) { $0 + $1.calories }
+    }
+    
+    // MARK: - Public Properties
+    
+    var mealIntake: MealIntake
+    
+    var recipes: [Recipe] {
+        viewModel.getMealIntakeRecipes(for: mealIntake)
+    }
+    
+    // MARK: - Body
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        mealIntakeInfoViewHeader
+            .padding(.top)
+        ScrollView {
+            LazyVStack {
+                ForEach(recipes, id: \.id) { recipe in
+                    SwipeToDeleteRowView {
+                        RecipeRowView(recipe: recipe, showAddButton: false)
+                    } deleteAction: {
+                        withAnimation(.easeInOut(duration: 0.4)) {
+                            viewModel.removeRecipeFromMealIntake(recipe, from: mealIntake)
+                        }
+                    } onTap: { }
+                }
+                .padding(.top, 10)
+                .padding(.horizontal)
+            }
+        }
+        .safeAreaInset(edge: .bottom, alignment: .center) {
+            addRecipeButton
+        }
     }
 }
 
-#Preview {
-    MealIntakeInfoView()
+// MARK: - Subviews
+
+private extension MealIntakeInfoView {
+    
+    private var mealIntakeInfoViewHeader: some View {
+        ZStack {
+            HStack {
+                VStack(alignment: .center) {
+                    Text(mealIntake.type)
+                        .font(.headline)
+                        .bold()
+                    Text("\(DateFormatterHelper.formatMediumDate(mealIntake.date))")
+                        .font(.caption)
+                }
+            }
+            HStack {
+                Image(systemName: "chevron.left")
+                    .font(.headline)
+                    .onTapGesture {
+                        dismiss.callAsFunction()
+                    }
+                    .padding(.leading)
+                Spacer()
+            }
+            .padding(10)
+            HStack {
+                Spacer()
+                Text(String(format: "%.0f", totalCalories))
+                Text("kcal")
+            }
+            .padding(.trailing)
+            .padding(10)
+            .bold()
+        }
+        .foregroundStyle(Color.theme.accentGreen)
+    }
+    
+    private var addRecipeButton: some View {
+        NavigationLink {
+            RecipeSearchView()
+                .environment(viewModel)
+                .navigationBarBackButtonHidden(true)
+        } label: {
+            Image(systemName: "plus")
+                .font(.title2)
+                .foregroundStyle(Color.white)
+                .padding()
+                .background(
+                    Circle()
+                        .fill(Color.theme.accentGreen)
+                        .shadow(color: Color.theme.accentGreen.opacity(0.7), radius: 10)
+                )
+        }
+    }
+    
+}
+
+// MARK: - Preview
+
+#Preview("Light Mode") {
+    MealIntakeInfoView(mealIntake: DeveloperPreview.instance.mealIntake)
+        .environment(DeveloperPreview.instance.nutritionViewModel)
+}
+
+#Preview("Dark Mode") {
+    MealIntakeInfoView(mealIntake: DeveloperPreview.instance.mealIntake)
+        .environment(DeveloperPreview.instance.nutritionViewModel)
+        .preferredColorScheme(.dark)
 }
